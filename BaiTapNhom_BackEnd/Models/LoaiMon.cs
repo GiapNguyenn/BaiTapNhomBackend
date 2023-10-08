6e6720d04e9801a0c13fb373d756c6ad9f5f18b7
tuana <tuana@LETUANANH>
2023-10-08 11:47:04 +0700

    Tuấn Anh đã hoàn thành

---
+++ BaiTapNhom_BackEnd/Models/LoaiMon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTapNhom_BackEnd.Models
+{
+    public class LoaiMon
+    {
+        [Key]
+        public int IDLoaiMon { get; set; }
+        [Required]
+        public string? TenLoaiMon { get; set; }
+        public string? Mota { get; set; }
+        public bool IsDelete { get; set; }
+
+    }
+}
---
+++ BaiTapNhom_BackEnd/Models/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BaiTapNhom_BackEnd.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<LoaiMon> loaiMons { get; set; }
+    }
+}
